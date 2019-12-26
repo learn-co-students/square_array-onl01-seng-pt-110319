64761cd6ddf71bcfd068b54a00254e7eaa5e5d71
Maika Kailani <queenmab051@gmail.com>
2019-12-26 16:18:46 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,8 @@
-def square_array(numbers)
-numbers = ['1,2,3']
-
-numbers.each do |n|
-  num1 = {numbers**2}
-  puts num1 
-end
-
-def square_array(new_numbers)
-new_numbers = [9,10,16,25]
-
-enu1 = (new_numbers).collect {|n| n ** 2}
-puts enu1
+def square_array(array)
+  new_arr = []
+  array.each do |x|
+    y = x**2
+    new_arr.push(y)
+  end
+  new_arr
 end
